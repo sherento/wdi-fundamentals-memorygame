de828ef8399d728f4c6694ba53f50e1bdd06b234
sheren <sheren.t@hotmail.com>
2017-10-22 08:02:35 +1100

    created game logic v2 u7

--- js/main.js
+++ js/main.js
@@ -1,27 +1,1 @@
-//console.log("Up and running!");
-//var cardOne = "queen";
-//var cardTwo = "queen";
-//var cardThree = "king";
-//var cardFour = "king";
-//console.log("User flipped" + " " + cardOne);
-//console.log("User flipped" + " " + cardTwo);
-
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-console.log("user flipped" + " " + "queen");
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("user flipped" + " " + "king");
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-  alert("you found a match!");
-} else {
-	alert("sorry, try again");
-	}
-};
+var cards = [{	rank: "queen",	suit: "hearts",	cardImage: "images/queen-of-hearts.png"},{	rank: "queen",	suit: "diamonds",	cardImage: "images/queen-of-diamonds.png"},{	rank: "king",	suit: "hearts",	cardImage: "images/king-of-hearts.png"},{	rank: "king",	suit: "diamonds",	cardImage: "images/king-of-diamonds.png"},];var cardsInPlay = [];var checkForMatch = function (){if (cardsInPlay.length === 2) {if (cardsInPlay[0] === cardsInPlay[1]) {console.log("You found a match!");} else {console.log("Sorry, try again.");}};var flipCard = function () {var cardId = this.getAttribute('data-id');console.log("user flipped " + cards[cardId].rank);cardsInPlay.push(cards[cardId].rank);this.setAttribute('src', cards[cardId].cardImage);checkForMatch ();console.log(cards[cardId].cardImage);console.log(cards[cardId].suit);}};var createBoard = function () {for (var i = 0; i < cards.length; i++) {var cardElement = document.createElement('img');cardElement.setAttribute('src', 'images/back.png');cardElement.setAttribute('data-id', i);cardElement.addEventListener('click', 'flipCard');document.getElementById('game-board').appendChild(cardElement);}};createBoard();
---
+++ js/main2.js
@@ -0,0 +1,9 @@
+console.log("up and running");
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("user flipped " + cardOne);
+console.log("user flipped " + cardThree);
