95d36982b8685941d91563037d92194ea58cdc3a
sheren <sheren.t@hotmail.com>
2017-10-22 20:29:48 +1100

    final

--- js/main.js
+++ js/main.js
@@ -27,9 +27,9 @@ var cardsInPlay = [];
 var checkForMatch = function() {
     if (cardsInPlay.length === 2) {
         if (cardsInPlay[0] === cardsInPlay[1]) {
-            console.log("You found a match!");
+            alert("You found a match!");
         } else {
-            console.log("Sorry, try again.");
+            alert("Sorry, try again.");
         }
     }
 };
